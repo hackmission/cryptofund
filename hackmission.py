fc39dacaca91aa4ca04e4a906d4b0b89e3363145
Helghardt Avenant <helghardt@gmail.com>
2017-07-27 14:36:57 +0200

    Read CSV

--- hackmission.py
+++ hackmission.py
@@ -1,11 +1,19 @@
 from rehive import Rehive, APIException
 from secret import REHIVE_API
+import csv
 
 rehive =  Rehive(REHIVE_API)
 
-rehive.auth.login(user="helghardt@rehive.com", company="hackmission", password="Belfius123")
-accounts = rehive.admin.users.get()
+# rehive.auth.login(user="helghardt@rehive.com", company="hackmission", password="Belfius123")
+# accounts = rehive.admin.users.get()
 
-transaction = rehive.admin.transactions.create_credit(user='helghardt@gmail.com', amount=10000, confirm_on_create=True)
+# transaction = rehive.admin.transactions.create_credit(user='helghardt@gmail.com', amount=10000, confirm_on_create=True)
 
-print(accounts)
+
+with open('cryptofund.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+    	print(row)
+    	print(row[0])
+
+# print(accounts)
